397cd8195b08ee941ef4ace57ffcfc6ad2736cf8
lunqwe <roma.boyko10068@gmail.com>
2024-07-09 18:40:46 +0300

    fixed issue

--- orders/models.py
+++ orders/models.py
@@ -3,16 +3,18 @@ from django.conf import settings
 
 from products.models import Product
 
-STATUS_CHOICES = {'pend': 'pending',
-                  'deliv': 'delivered',
-                  'prog': 'in progress'
-                  }
+
+class Status(models.TextChoices):
+        PENDING = 'pend', 'Pending'
+        DELIVERED = 'deliv', 'Delivered'
+        IN_PROGRESS = 'prog', 'In Progress'
 
 # Create your models here.
 class Order(models.Model):
+
     owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_DEFAULT, default="Anonymous user")
     products = models.ManyToManyField(Product)
     total_cost = models.FloatField()
-    status = models.TextChoices(STATUS_CHOICES)
+    status = models.CharField(max_length=10, choices=Status.choices, default=Status.PENDING)
     deliver_to = models.CharField(max_length=999)
     
