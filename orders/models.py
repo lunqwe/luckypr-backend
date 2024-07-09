f9c2323c30d7c99f2accc6f04f5fc8f07805777a
lunqwe <roma.boyko10068@gmail.com>
2024-07-09 18:37:44 +0300

    fix

--- orders/models.py
+++ orders/models.py
@@ -10,8 +10,9 @@ STATUS_CHOICES = {'pend': 'pending',
 
 # Create your models here.
 class Order(models.Model):
-    owner = models.ForeignKey(settings.AUTH_USER_MODEL)
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_DEFAULT, default="Anonymous user")
     products = models.ManyToManyField(Product)
     total_cost = models.FloatField()
     status = models.TextChoices(STATUS_CHOICES)
+    deliver_to = models.CharField(max_length=999)
     
